c697fccadf083865029d7914a1db9bd7ce6bdf28
Vladislav Bondarchuk <vladiksun85@gmail.com>
2017-01-16 15:15:37 +0400

    Implementation

---
+++ module1-solution/app.js
@@ -0,0 +1,30 @@
+(function () {
+    'use strict';
+
+    angular.module('LunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController);
+
+
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController ($scope) {
+
+        $scope.checkItems = function () {
+            $scope.items = [];
+
+            if ($scope.textInput){
+                $scope.items = $scope.textInput.split(',');
+            }
+
+            if ($scope.items.length == 0){
+                $scope.message = 'Please enter data first!';
+            }
+            else if ($scope.items.length <= 3) {
+                $scope.message = 'Enjoy!';
+            }
+            else {
+                $scope.message = 'Too much!';
+            }
+        }
+    }
+})();
+
