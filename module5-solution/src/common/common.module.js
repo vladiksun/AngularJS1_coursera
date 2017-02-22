78058898db04e1f75af4bdecae9bdb91dc8584cc
Vladislav Bondarchuk <vladiksun85@gmail.com>
2017-02-22 17:59:25 +0400

    ng-cloak directive added to avoid  html template from being briefly displayed by the browser in its raw (uncompiled) form

--- module5-solution/src/common/common.module.js
+++ module5-solution/src/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'https://davids-restaurant.herokuapp.com')
+.constant('ApiPath', 'https://vladiksun-course5.herokuapp.com')
 .config(config);
 
 config.$inject = ['$httpProvider'];
