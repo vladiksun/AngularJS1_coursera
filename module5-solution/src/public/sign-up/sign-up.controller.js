b2086ca2bf8dc7163dcf0861318c0a81eeea577e
Vladislav Bondarchuk <vladiksun85@gmail.com>
2017-02-22 17:42:32 +0400

    module 5 solution

---
+++ module5-solution/src/public/sign-up/sign-up.controller.js
@@ -0,0 +1,12 @@
+(function () {
+    "use strict";
+
+    angular.module('public')
+        .controller('SignUpController', SignUpController);
+
+    function SignUpController() {
+        var $ctrl = this;
+
+    }
+
+})();
